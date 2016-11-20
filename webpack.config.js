08cc217f9834a2cabc120e31da80745b7a7a9e44
Narazaka <info@narazaka.net>
2016-11-20 14:35:38 +0900

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const config = require("webpack-config-narazaka-ts-js").node;
 
-config.entry.shiori_converter = "./src/lib/shiori_transaction.ts";
+config.entry.shiori_transaction = "./src/lib/shiori_transaction.ts";
 config.output.library = "shioriTransaction";
 
 module.exports = config;
--- webpack.config.web.js
+++ webpack.config.web.js
@@ -1,6 +1,6 @@
 const config = require("webpack-config-narazaka-ts-js").web;
 
-config.entry.shiori_converter = "./src/lib/shiori_transaction.ts";
+config.entry.shiori_transaction = "./src/lib/shiori_transaction.ts";
 config.output.library = "shioriTransaction";
 
 module.exports = config;
